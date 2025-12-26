1266a5cb1f5b148adfa45c2bb5bc404d96dcddf3
behnooshbhy2002 <behnoosh.bhy@gmail.com>
2025-12-26 23:22:33 +0330

    Using Redirect

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,21 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+
+monthly_challenges = {
+    "january": "Eat no meat for entire month",
+    "february": "Walk for at least 20 minutes every day",
+    "march": "Learn Django for at least 20 minutes every day",
+    "april": "Read 10 pages of a book every day",
+    "may": "Drink at least 8 glasses of water every day",
+    "june": "Do 15 minutes of stretching every day",
+    "july": "Write 5 minutes of journaling every day",
+    "august": "Learn 10 new English words every day",
+    "september": "No sugar for the entire month",
+    "october": "Meditate for at least 10 minutes every day",
+    "november": "Save money: no unnecessary spending",
+    "december": "Message or call one friend/family member every day"
+}
 
 # Create your views here.
 def monthly_challenge_by_number(request, month):
-    return HttpResponse(month)
+    months = list(monthly_challenges.keys())
+
+    if month > len(months):
+        HttpResponseNotFound("Invalid number")
+
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
 
 def monthly_challenge(request, month):
     challenge_text = None
-    if month == "january":
-        challenge_text = "Eat no meat for entire month"
-    elif month == "february":
-        challenge_text = "Walk for at least 20 minutes"
-    elif month == "march":
-        challenge_text = "Learn Django"
-    elif month == "april":
-        challenge_text = "Do your best"
-    else:
+    try: 
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
         return HttpResponseNotFound("This month is not supported")
-
-    return HttpResponse(challenge_text)
+    
